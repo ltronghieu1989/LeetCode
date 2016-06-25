5c9c0e9210461f4fcedbce272447af578c6dc2a7
ltronghieu1989 <ltronghieu1989@gmail.com>
2016-06-25 12:19:24 -0700

    update

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,3 +1,46 @@
-public class LeetCode {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class LeetCode {
+    /*
+    Find least occurrence in a given string
+     */
+    public char firstLeastOccurence(String s) {
+        char ans = ' ';
+        if (s == null || s.length() == 0) return ans;
+
+        int[] arr = new int[256];
+        Arrays.fill(arr, -1);
+        char[] letters = s.toCharArray();
+
+        for (int i = 0; i < letters.length; i++) {
+            char c = letters[i];
+            if (arr[c - '0'] == -1)
+                arr[c - '0'] = i;
+        }
+
+        Arrays.sort(letters);
+
+        int count = 1;
+        int minIdx = Integer.MAX_VALUE;
+        for (int i = 1; i < letters.length; i++) {
+            if (letters[i - 1] != letters[i]) {
+                if (count == 1) {
+                    minIdx = Math.min(minIdx, arr[letters[i - 1] - '0']);
+                }
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        if (count == 1) {
+            minIdx = Math.min(minIdx, arr[letters[letters.length - 1] - '0']);
+        }
+
+        if (minIdx != Integer.MAX_VALUE) ans = s.charAt(minIdx);
+
+        return ans;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    private static LeetCode lc = new LeetCode();
+    private static TopCoder tc = new TopCoder();
+
+    public static void main(String[] args) {
+        char ans = lc.firstLeastOccurence("abbcdcdea");
+        System.out.println(ans);
+    }
 }
