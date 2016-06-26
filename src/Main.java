8c2d61923b0d019292dc79bb65ff655cc0d23edd
ltronghieu1989 <ltronghieu1989@gmail.com>
2016-06-25 18:47:49 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
     private static LeetCode lc = new LeetCode();
     private static TopCoder tc = new TopCoder();
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
 //        String fronts = "abc,abc,def,cba,fed";
 //        int[] backs = {24, 7, 63, 222, 190};
 
-        String fronts = "aabaa,defg";
-        int[] backs = {99, 1};
+        String fronts = "aaaaaaaaaaaa,nopalindrome,steponnopets,emordnilapon,aaaaaaaaaaaa,steponnopets,nopalindrome,steponnopets,nopalindrome,bbbbbbbbbbbb,cannotbeused,cannotbeused,steponnopets,aaaaaaaaaaaa,nopalindrome,aaaaaaaaaaaa,nopalindrome,emordnilapon,steponnopets,nopalindrome";
+        int[] backs = {4096, 131072, 64, 262144, 512, 1024, 65536, 2048, 32768, 1, 524288, 16384, 32, 4, 16, 2, 8, 128, 8192, 256};
+
+//        String fronts = "aabaa,defg";
+//        int[] backs = {99, 1};
 
         int ans = tc.PalindromeGame(fronts.split(","), backs);
         System.out.println(ans);
