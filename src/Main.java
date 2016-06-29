1b7c71b7ee37b0dc56423efa9a744cb269f6bfaa
ltronghieu1989 <ltronghieu1989@gmail.com>
2016-06-29 11:52:50 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,7 @@ public class Main {
     private static LeetCode lc = new LeetCode();
     private static TopCoder tc = new TopCoder();
 
-    public static void main(String[] args) {
-
-        System.out.println(1 << 4);
-    }
-
-    public static void preorderTraversal(TreeNode root) {
+    private static void preorderTraversal (TreeNode root) {
         if (root != null) {
             System.out.printf("%d ", root.val);
             preorderTraversal(root.left);
@@ -17,7 +12,7 @@ public class Main {
         }
     }
 
-    public static void postorderTraversal(TreeNode root) {
+    private static void postorderTraversal (TreeNode root) {
         if (root != null) {
             postorderTraversal(root.left);
             postorderTraversal(root.right);
@@ -25,11 +20,35 @@ public class Main {
         }
     }
 
-    public static void inorderTraversal(TreeNode root) {
+    private static void inorderTraversal (TreeNode root) {
         if (root != null) {
             inorderTraversal(root.left);
             System.out.printf("%d ", root.val);
             inorderTraversal(root.right);
+        }
+    }
+
+    private static String DecToBinary (int i) {
+        StringBuilder sb = new StringBuilder();
+        while (i > 0) {
+            sb.insert(0, (i & 1) == 1 ? "1" : "0");
+            i = i >> 1;
+        }
+        while (sb.length() < 4) {
+            sb.insert(0, "0");
+        }
+        return sb.toString();
+    }
+
+    public static void main (String[] args) {
+
+        List<Integer> ans = lc.grayCode(4);
+        for (Integer i : ans) {
+            System.out.printf("%d\t\t", (i));
+        }
+        System.out.println();
+        for (Integer i : ans) {
+            System.out.printf("%s\t", DecToBinary(i));
         }
     }
 }
